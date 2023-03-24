35b2ce0a62dfe195a2ccd9f17681d9500ca33b73
aditisg06 <124760046+aditisg06@users.noreply.github.com>
2023-03-24 10:18:29 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+btn.addEventListener('click', function onClick(event){
+        document.body.style.backgroundColor = 'white';
+        document.body.style.color = '#66a7f4';
+        document.getElementsByClassName("skills-col").style.marginLeft="100px";
+});
